798078fec8d5e8649f6a16bfe8627c3d38acfefd
macnick <nick@macexperts.gr>
2019-09-09 18:49:15 +0300

    Added my_each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -7,9 +7,10 @@ module Enumerable
     end
   
     def my_each_with_index
-    
-    
-    
+        for i in 0...self.length
+                yield self[i], i
+            end
+        end
     end
 
 
