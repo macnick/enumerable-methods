a69f4b631c42d213cad0e5484d4295e2c846b2f1
macnick <nick@macexperts.gr>
2019-09-10 17:40:22 +0300

    Fixed #my_each_with_index to return an enumerator instance when no block is given

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -8,14 +8,18 @@ module Enumerable
         else
             to_enum(:my_each)
         end
-        end
-  
-  def my_each_with_index
-    for i in 0...self.length
-        yield self[i], i
-      end
     end
-  end
+  
+    def my_each_with_index
+        if block_given?
+          self.my_each { |e, i = 0|
+              yield self[i], i
+              i+=1
+          }
+        else
+          to_enum(:my_each_with_index)
+        end
+      end
 
   def my_select
     result = []
