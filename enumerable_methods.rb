1920d23ada0197067dab6f6da087a86c6657d24a
macnick <nick@macexperts.gr>
2019-09-10 17:30:55 +0300

    Fixed #my_each to return an enumerator instance when no block is given

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,20 +1,51 @@
 module Enumerable
 
     def my_each
-        for i in 0...self.length
+        if block_given?
+            for i in 0...self.length
             yield self[i]
-        end
-    end
-  
-    def my_each_with_index
-        for i in 0...self.length
-                yield self[i], i
             end
+        else
+            to_enum(:my_each)
         end
+        end
+  
+  def my_each_with_index
+    for i in 0...self.length
+        yield self[i], i
+      end
     end
+  end
 
+  def my_select
+    result = []
+    self.my_each {|e| 
+      result << e if yield(e)
+    }
+    result
+  end
 
+  def my_all?
 
+  end
 
+  def my_any?
+  
+  end
+
+  def my_none?
+  end
+
+  def my_count
+  end
+
+  def my_map
+  end
+
+  def my_inject
+  end
+
+  def multiply_els
+  end
 
 end
