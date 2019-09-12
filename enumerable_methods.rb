1d8dd489949c675435bb08f173ec77cff8a21f5f
macnick <nick@macexperts.gr>
2019-09-12 14:24:34 +0300

    Created a super #my_inject method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -80,16 +80,49 @@ module Enumerable
             to_enum(:my_map)
         end
       end
-      
+
   # methods below this line are not ready yet 
     
-  def my_map
-  end
-
   def my_inject
+    
   end
 
   def multiply_els
   end
 
 end
+
+# TESTS
+# p [10, 18, 4, 6, 4, 16, 5].map {|e| e * 2}
+# p [10, 18, 4, 6, 4, 16, 5].my_map {|e| e * 2}
+# p [10, 18, 4, 6, 4, 16, 5].map
+# p [10, 18, 4, 6, 4, 16, 5].my_map
+ 
+# p [10, 18, 4, 6, 4, 16, 5, 8].count 
+# p [10, 18, 4, 6, 4, 16, 5, 8].my_count
+  
+# p [10, 18, 4, 6, 4, 16, 5, 8].count(1)
+# p [10, 18, 4, 6, 4, 16, 5, 8].my_count(1)
+
+# p [10, 18, 4, 6, 14, 16, 5, 8].count { |x| x%2==0 }
+# p [10, 18, 4, 6, 14, 16, 5, 8].my_count { |x| x%2==0 }
+
+# p [10, 18, 4, 6, 14, 16, 5, 8].none? { |e| e < 0 }
+# p [10, 18, 4, 6, 14, 16, 5, 8].my_none? { |e| e < 0 }
+# p "without block"
+# p [nil, nil, nil].none? 
+# p [nil, nil, nil].my_none? 
+# p [nil, nil, 5].none? 
+# p [nil, nil, 5].my_none? 
+
+# p [10, 18, 4, 6, 14, 16, 5, 8].any? { |e| e == 17 }
+# p [10, 18, 4, 6, 14, 16, 5, 8].my_any? { |e| e == 17 }
+# p "without block"
+# p [nil, nil].any? 
+# p [nil, nil].my_any?  
+
+# p [10, 18, 4, 6, 14, 16, 5, 8].all? { |e| e >= 0 }
+# p [10, 18, 4, 6, 14, 16, 5, 8].my_all? { |e| e >= 0 }
+# p "without block"
+# p [10, 18, 4, 6, 14, 16, nil, 8].all? 
+# p [10, 18, 4, 6, 14, 16, nil, 8].my_all? 
