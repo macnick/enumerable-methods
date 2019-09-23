569a804dff8971eb5851ff4c7b727722fa18054a
macnick <nick@macexperts.gr>
2019-09-23 19:13:43 +0300

    Fixed bugs in my_all?, my_any?, my_none? methods

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -11,16 +11,6 @@ module Enumerable
     end
   end
 
-  # def my_each_with_index
-  #   i = 0
-  #   arr = []
-  #   while i < size
-  #     arr << yield(self[i], i)
-  #     i += 1
-  #   end
-  #   arr
-  # end
-
   def my_each_with_index
     if block_given?
       my_each do |_e, i = 0|
@@ -43,25 +33,36 @@ module Enumerable
     end
   end
 
-  def my_all?
+  def my_all?(pattern = nil)
+    unless pattern.nil?
+      return false if instance_of?(Hash)
+
+      return my_all? { |e| pattern.match(e) }
+    end
+
     return my_all? { |e| e } unless block_given?
 
-    my_each { |e| return false unless yield e }
+    my_each { |e| return false unless yield(e) }
     true
   end
 
-  def my_any?
+  def my_any?(pattern = nil)
+    unless pattern.nil?
+      return false if instance_of?(Hash)
+
+      return my_any? { |e| pattern.match(e) }
+    end
+
     return my_any? { |e| e } unless block_given?
 
-    my_each { |e| return true if yield e }
+    my_each { |e| return true if yield(e) }
+    false
   end
 
   def my_none?(*pattern)
-    if block_given?
-      !my_any? { |e| yield e }
-    else
-      !my_any?(*pattern)
-    end
+    return !my_any?(*pattern) unless block_given?
+
+    !my_any? { |e| yield(e) }
   end
 
   def my_count(arg = nil)
@@ -88,20 +89,30 @@ module Enumerable
     end
   end
 
-  def my_inject(stv = nil, sym = nil, &block)
-    # with checks it can handle everything
-    stv = stv.to_sym if stv.is_a?(String) && !sym && !block
-    if stv.is_a?(Symbol) && !sym
-      block = stv.to_proc
-      stv = nil
-    end
-    sym = sym.to_sym if sym.is_a?(String)
-    block = sym.to_proc if sym.is_a?(Symbol)
+  def my_inject(stv = nil, sym = nil)
+    # make sure it can take custom stv (starting value) and symbol procs like :+
+    return my_inject(nil, stv) if stv.is_a? Symbol
 
-    # Ready to rock & roll
-    each { |x| stv = stv.nil? ? x : block.yield(stv, x) }
+    return my_inject(stv) { |mem, e| :+.to_proc.call(mem, e) } unless sym.nil?
+
+    my_each { |e| stv = stv.nil? ? first : yield(stv, e) }
     stv
   end
+
+  # def my_inject(stv = nil, sym = nil, &block)
+  #   # with checks it can handle everything
+  #   stv = stv.to_sym if stv.is_a?(String) && !sym && !block
+  #   if stv.is_a?(Symbol) && !sym
+  #     block = stv.to_proc
+  #     stv = nil
+  #   end
+  #   sym = sym.to_sym if sym.is_a?(String)
+  #   block = sym.to_proc if sym.is_a?(Symbol)
+
+  #   # Ready to rock & roll
+  #   each { |x| stv = stv.nil? ? x : block.yield(stv, x) }
+  #   stv
+  # end
 end
 
 def multiply_els(arr)
@@ -117,15 +128,15 @@ end
 
 # p multiply_els([2,4,5])
 
-# p [1,2,3,4,5].reduce(2,&:+)
-# p [1,2,3,4,5].my_inject(2,&:+)
-# p [1,2,3,4,5].my_inject(:*)
-# p [1,2,3,4,5].my_inject(2, &:*)
+# p [1, 2, 3, 4, 5].reduce(2,&:+)
+# p [1, 2, 3, 4, 5].my_inject(2,&:+)
+# p [1, 2, 3, 4, 5].my_inject(&:*)
+# p [1, 2, 3, 4, 5].my_inject(2, &:*)
 # p %w(3 4 5).my_inject(&:+)
 # p %w(3 4 5).my_inject("hello", &:+)
 
-# p [1,2,3,4,5].my_inject { |sum, n| sum * n }
-# p [1,2,3,4,5].inject { |sum, n| sum * n }
+# p ([1, 2, 3, 4, 5]).my_inject { |sum, n| sum * n }
+# p [1, 2, 3, 4, 5].inject { |sum, n| sum * n }
 
 # p [10, 18, 4, 6, 4, 16, 5].map {|e| e * 2}
 # p [10, 18, 4, 6, 4, 16, 5].my_map {|e| e * 2}
@@ -148,31 +159,36 @@ end
 # p [nil, nil, nil].my_none?
 # p [nil, nil, 5].none?
 # p [nil, nil, 5].my_none?
-
+# p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+# p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+# p %w{ant bear cat}.my_none?(/d/)                        #=> true
+# p [].my_none?                                           #=> true
+# p [nil].my_none?                                        #=> true
+# p [nil, false].my_none?                                 #=> true
+# p [nil, false, true].my_none?  # false
 # p [10, 18, 4, 6, 14, 16, 5, 8].any? { |e| e == 17 }
 # p [10, 18, 4, 6, 14, 16, 5, 8].my_any? { |e| e == 17 }
 # p "without block"
 # p [nil, nil].any?
 # p [nil, nil].my_any?
+# p "with regex"
+# p %w[ant bear cat].any?(/d/)
+# p %w[ant bear cat].my_any?(/d/)
+# p [nil, true, 99].any?
+# p [nil, true, 99].my_any?
 
 # p [10, 18, 4, 6, 14, 16, 5, 8].all? { |e| e >= 0 }
 # p [10, 18, 4, 6, 14, 16, 5, 8].my_all? { |e| e >= 0 }
 # p 'without block'
 # p [10, 18, 4, 6, 14, 16, nil, 8].all?
 # p [10, 18, 4, 6, 14, 16, nil, 8].my_all?
-p %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
-p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
-p %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
-p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
 
-p %w[ant bear cat].all?(/t/)                        #=> false
-p %w[ant bear cat].my_all?(/t/)                        #=> false
-p [1, 2i, 3.14].all?(Numeric)                       #=> true
-p [1, 2i, 3.14].my_all?(Numeric)
-p [nil, true, 99].all?                              #=> false
-p [nil, true, 99].my_all?
-p [].all?                                           #=> true
-p [].my_all?
+# p %w[ant bear cat].all?(/t/)
+# p %w[ant bear cat].my_all?(/t/)
+# p [nil, true, 99].all?
+# p [nil, true, 99].my_all?
+# p [].all?
+# p [].my_all?
 # hash = Hash.new
 # %w(cat dog wombat).each_with_index { |item, index|
 #   hash[item] = index
@@ -181,6 +197,6 @@ p [].my_all?
 # %w(cat dog wombat).my_each_with_index { |item, index|
 #   hash[item] = index
 # }
-# p hash 
+# p hash
 # p %w(cat dog wombat).each_with_index
 # p %w(cat dog wombat).my_each_with_index
