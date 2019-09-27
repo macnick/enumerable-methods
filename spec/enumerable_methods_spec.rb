3aa924f83cc385e83c97353174e00a5f35fdd4b6
macnick <nick@macexperts.gr>
2019-09-27 17:55:21 +0300

    fix my_aech test

--- spec/enumerable_methods_spec.rb
+++ spec/enumerable_methods_spec.rb
@@ -3,28 +3,35 @@
 require '../enumerable_methods'
 
 RSpec.describe Enumerable do
- describe 'my_each' do
-    context 'When passed an array' do
-      it 'calls the given block once for each element in self, passing that element as a parameter.' do
-        arr = %w[a b c]
-        expect(arr.each { |x| print x, ' -- ' }).to eql('a -- b -- c --')
-        expect(arr.each).to eql(0...3)
+  let(:array) { [1, 2, 3, 4] }
+  let(:str_to_array) { %w[cat dog wombat] }
+  let(:result) { [] }
+  describe 'my_each' do
+      context 'When passed an array' do
+        it 'calls the given block once for each element in self, passing that element as a parameter.' do
+          array.my_each { |x| result << x}
+          expect(result).to eql([1, 2, 3, 4])
+        end
+        it 'if no block is given, an Enumerator is returned' do
+          expect(array.my_each).to be_a(Enumerator)
+        end
+        it 'returns an array' do
+          expect(array.my_each.to_a).to eq(array)
+        end
       end
     end
-  end
-  describe '#my_each_with_index' do
-    let(:array) { [1, 2, 3, 4] }
-    let(:str_to_array) { %w[cat dog wombat] }
-    let(:result) { [] }
-    it 'returns indexes of elements in array' do
-      array.my_each_with_index { |elem, index| result << index }
-      expect(result).to eql([0, 1, 2, 3])
-    end
 
-    it 'returns indexes of elements in our array' do
-      str_to_array.my_each_with_index { |el, ind| result << ind }
-      expect(result).to eql([0, 1, 2])
-    
+  describe '#my_each_with_index' do
+    context 'When passed an array' do
+      it 'returns indexes of elements in array' do
+        array.my_each_with_index { |elem, index| result << index }
+        expect(result).to eql([0, 1, 2, 3])
+      end
+
+      it 'returns indexes of elements in our array' do
+        str_to_array.my_each_with_index { |el, ind| result << ind }
+        expect(result).to eql([0, 1, 2])
+      end
     end
   end
 end
